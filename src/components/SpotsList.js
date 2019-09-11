088980d73490e09dd06ec5bd81cd0f0fe2e1b9f1
Juergen Baltres <juergen.baltres@gmail.com>
2019-09-11 12:10:34 +0200

    create structure with placeholder componnents

---
+++ src/components/SpotsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Spot from "./Spot";
+
+function FilterList() {
+  return (
+    <div>
+      Spotslist <Spot />
+    </div>
+  );
+}
+
+export default FilterList;
