e83639873c9ff98c5bf1148c97a3ad0f88182d68
Juergen Baltres <juergen.baltres@gmail.com>
2019-09-12 08:56:21 +0200

    create single card layout

--- src/components/Spot.js
+++ src/components/Spot.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Spot() {
-  return <div className="spot"></div>;
+  return (
+    <div className="spot">
+      <img
+        className="spot__img"
+        src="https://rp-online.de/imgs/32/2/2/4/4/1/9/0/7/tok_e310a54e0e8dc30bde7bb2595d8ba812/w940_h528_x470_y264_1b70c410752b73ef.jpg"
+        alt="Description"
+      />
+      <div className="spot__content">
+        <h3>Kölner Dom</h3>
+        <div>Kategorie</div>
+
+        <span>Entfernung</span>
+        <span>Bewertung</span>
+        <details>
+          Hier steht etwas geiles drinn was aber keiner lesen kann.
+          <summary>Super Beschreibungstext 🦉</summary>
+        </details>
+      </div>
+    </div>
+  );
 }
 
 export default Spot;
