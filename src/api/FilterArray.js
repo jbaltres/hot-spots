ddce922befedcb3ca727d4e20c693a59be9c6dd8
Jürgen Baltres <jbaltres@Jurgens-MBP.fritz.box>
2020-10-27 08:14:42 +0100

    add real values in database

--- src/api/FilterArray.js
+++ src/api/FilterArray.js
@@ -1,20 +1,14 @@
 export const filterArray = [
   {
     description: "Land",
-    options: ["deutschland", "schweiz", "portugal", "spanien"]
+    options: ["Deutschland", "Großbritanien", "Japan", "Schottland", "Spanien"]
   },
   {
     description: "Preis",
     options: ["0€-10€", "10€-15€", "15€-20€","20€-30€", "30€-50€", "50€-100€",">100€"]
   },
-
   {
-    description: "Geschmack",
-    options: [
-      "süß",
-      "fruchtig",
-      "normal",
-      "kräftig"
-    ]
-  }
+    description: "GinKategorie",
+    options:["London Dry Gin","Dry Gin / Destillierter Gin"," (Sloe Gin)","New Western Dry Gin","Old Tom Gin","Reserve Gin / Aged Gin","Cordial Gin","Pink Gin"]
+  },
 ];
--- src/api/SpotArray.js
+++ src/api/SpotArray.js
@@ -3,65 +3,142 @@
 export const gins = [
   {
     imgSrc:
-      "https://wacholder-express.de/media/image/75/3f/2d/Studer-Classic_600x600@2x.jpg",
-    title: "Studer Swiss Classic Gin",
-    country: "schweiz",
-    price: 42,
-    taste: "normal",
-    description: "Ingwer, Koriander, Kubeben-Pfeffer, Lavendel, Wacholder, Zitronengras"
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Monkey 47 Schwarzwald Gin",
+    brand:"Black Forest Distillers GmbH",
+    country: "Deutschland",
+    price: 65,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Wacholder","Zitrus","Würzig","Kräuter"],
+    description: "Beerenaromen/ 47% vol.",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-monkey-47-schwarzwald-dry-gin-3934/"
   },
   {
     imgSrc:
-      "https://wacholder-express.de/media/image/bf/aa/c6/Blue-Magic_600x600@2x.jpg",
-    title: "Sharish Blue Magic Gin",
-    country: "portugal",
-    price: 27,
-    taste: "fruchtig",
-    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt"
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Gin Mare", 
+    brand: "Global Premium Brands, S.A.",
+    country: "Spanien",
+    price: 41,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Zitrus","Wacholder","Kräuter","Floral"],
+    description: "42.7% vol/ mediterrane Kräuter",
+    link: "https://www.spirituosen-journal.de/spirituosen-test-gin-mare-31808/"
   },
   {
     imgSrc:
-      "https://wacholder-express.de/media/image/cb/17/7a/360_Gin_RotadoSul_1000x1000_600x600@2x.jpg",
-    title: "Gin Sul Edition Rota do Sul",
-    country: "deutschland",
-    price: 299,
-    taste: "kräftig",
-    description: "Die Algarve in Portugal mit ihren vielen Sonnentagen, dem heißen und trockenen Sommer und der sanften Meerbrise gilt als die südlichste Region auf dem portogiesischen Festland und spielt bei dem Gin Sul eine große Rolle. Gleiches gilt auch für die Sonderedition von 2016, den Gin Sul in der Edition Rota do Sul. Die Hersteller dieses Gins waren von der Vegetation der portogiesischen Küste so begeistert, dass sie sich kurzerhand aufmachten, um in der Region nach weiteren Botanicals zu suchen, die sie für ihre Gins verwenden konnten. Die Zistrose, eine Art Strauch, wächst im heißen Klima des Mittelmeers und an der Atlantikküste Portugals. Diese Zutat findet bereits im klassischen Gin Sul Verwendung. Da verwundert es nicht, dass die Hersteller des Gins für den Rota do Sul erneut die sonnenbeschienene Region aufsuchten. Die ausgewählten Botanicals für diesen Gin spiegeln das Flair der portogiesischen Küste perfekt wider. Oregano, Thymianblüten und Rosmarin verleihen dem Rota do Sul eine kräuterige Note, während Strohblumen den Gin mit ihren floralen Aromen untermalen. Frische Zitronen, Apfelsinen und Aprikosen sorgen für eine angenehme Frische, die nicht nur im heißen Klima der Algarve äußerst willkommen ist. In der Altoaner Spirituosen Manufaktur in Hamburg wurde schließlich klares Quellwasser aus dem Monchique-Gebirge hinzugegeben, das dem Rota do Sul noch mehr Exklusivität verleiht. Der Gin ist außerdem auf nur 2.000 Flaschen limitiert."
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Hendricks", 
+    brand: "William Grant & Sons",
+    country: "Schottland",
+    price: 39,
+    category: "New Western Dry Gin",
+    taste: ["Zitrus","Wacholder","Süßlich","Floral"],
+    description: "44% vol/ Rosen und Gurkenaroma",
+    link: "https://www.spirituosen-journal.de/spirituosen-test-hendricks-gin-7841/"
   },
   {
     imgSrc:
-      "https://wacholder-express.de/media/image/23/35/66/Goldcap-2018_600x600@2x.jpg",
-    title: "Ferdinand's Saar Dry Gin Goldcap 2018 in Messingschatulle",
-    country: "deutschland",
-    price: 27,
-    taste: "kräftig",
-    description: "geheime Zutaten, Riesling"
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "The Duke Munich Dry Gin", 
+    brand: "The Duke Destillerie",
+    country: "Deutschland",
+    price: 38,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Wacholder","Süßlich","Floral"],
+    description: "45% vol/ Lavendel, Hopfen, Malz",
+    link: "https://www.spirituosen-journal.de/spirituosen-test-the-duke-munich-dry-gin-1307/"
   },
   {
     imgSrc:
-      "https://wacholder-express.de/media/image/f2/37/fc/Procera-Gin_600x600@2x.jpg",
-    title: "Teuer Gin",
-    country: "portugal",
-    price: 89,
-    taste: "fruchtig",
-    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt"
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "The Duke Rough Gin", 
+    brand: "The Duke Destillerie",
+    country: "Deutschland",
+    price: 34,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Wacholder","Fruchtig","Würzig","Herb"],
+    description: "42% vol/ Kräftiger Wacholder, Hopfen, Malz",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-the-duke-rough-56195/"
   },
   {
-  imgSrc:
-      "https://wacholder-express.de/media/image/03/fe/04/Gin-Mare-Mini_600x600@2x.jpg",
-    title: "Gin Mare",
-    country: "spanien",
-    price: 9,
-    taste: "kräftig",
-    description: "Basilikum, Oliven, Rosmarin, Thymian"
-  },  
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "The Duke Wanderlust Gin", 
+    brand: "The Duke Destillerie",
+    country: "Deutschland",
+    price: 48,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Zitrus","Fruchtig","Floral","Herb"],
+    description: "47% vol/  Himbeernoten, Hopfen, Malz",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-the-duke-wanderlust-56298/",
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Ferdinand's Saar Dry Gin", 
+    brand: "Capulet and Montague",
+    country: "Deutschland",
+    price: 61,
+    category: "London Dry Gin",
+    taste: ["Zitrus","Kräuter","Floral"],
+    description: "44% vol/  Rieslingtrauben",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-ferdinands-saar-dry-gin-30089/"
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Galeb Dry Gin", 
+    brand: "CRO-Distillers, Zubac & Zink GbR",
+    country: "Deutschland",
+    price: 0,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Zitrus","Fruchtig","Süßlich"],
+    description: "43% vol/  fruchtige kroatische Feige",
+    link: "nD"
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Roku Gin", 
+    brand: "Suntory",
+    country: "Japan",
+    price: 32,
+    category: "Dry Gin / Destillierter Gin",
+    taste: ["Zitrus","Würzig","Floral"],
+    description: "43% vol/  Kirschblüten, Grüner Tee, Yuzu",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-roku-gin-2-58905/"
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Windspiel Premium Dry Gin", 
+    brand: "Windspiel Manufaktur GmbH",
+    country: "Deutschland",
+    price: 71,
+    category: "London Dry Gin",
+    taste: ["Zitrus","Süßlich","Floral"],
+    description: "47% vol/  Rohalkohol aus heimischen Kartoffeln",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-windspiel-dry-gin-30522/"
+  },
+  {
+    imgSrc:
+      "https://images.unsplash.com/photo-1530915174962-4fdb8a8d5e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Tanqueray London Dry Gin", 
+    brand: "CHARLES TANQUERAY & CO. NORWALK, CT.",
+    country: "Großbritanien",
+    price: 22,
+    category: "London Dry Gin",
+    taste: ["Zitrus","Würzig","Wacholder","Trocken"],
+    description: "47,3% vol/  Stark, Bittere Noten",
+    link:"https://www.spirituosen-journal.de/spirituosen-test-tanqueray-london-dry-gin-34532/"
+  },
   
 ];
 
 export const tonicwaters = [
   {
-    imgSrc:
-      "https://www.rheinspirits.com/wp-content/uploads/2018/02/Thomas-Henry-Tonic-02l.jpg",
+    imgSrc:"https://images.unsplash.com/photo-1598614187854-26a60e982dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Rheinspirit",
     country: "deutschland",
     price: 1,
@@ -70,17 +147,7 @@ export const tonicwaters = [
     gin: ["Gin Mare", "Teuer Gin","Ferdinand's Saar Dry Gin Goldcap 2018 in Messingschatulle","Studer Swiss Classic Gin","Sharish Blue Magic Gin"]
   },
   {
-    imgSrc:
-      "https://www.durstretter.de/media/image/product/185/lg/schweppes-indian-tonic-water-6x10l-mehrweg.jpg",
-    title: "Indian",
-    country: "portugal",
-    price: 2.70,
-    taste: "fruchtig",
-    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
-    gin: ["Gin Mare", "Teuer Gin","Gin Sul Edition Rota do Sul"]
-  },
-  {
-    imgSrc: "https://www.novado.de/pub/media/catalog/product/cache/db563f2302460f8ef73bd3b1c97ac808/g/o/goldberg-_tonic_water-_0_20_glasflasche_3.jpg",
+    imgSrc: "https://images.unsplash.com/photo-1598614187854-26a60e982dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Schweppes",
     country: "deutschland",
     price: 29.99,
@@ -94,34 +161,31 @@ export const tonicwaters = [
 
 export const ingredients = [
   {
-    imgSrc: "https://images.eatsmarter.de/sites/default/files/styles/facebook/public/warenkunde-nelken-300x225.jpg",
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Nelke",
     country: "deutschland",
     price: 1,
-    taste: "normal",
     description: "Ingwer, Koriander, Kubeben-Pfeffer, Lavendel, Wacholder, Zitronengras",
     gin: ["Gin Mare", "Teuer Gin","Ferdinand's Saar Dry Gin Goldcap 2018 in Messingschatulle","Studer Swiss Classic Gin","Sharish Blue Magic Gin"]
   },
   {
-    imgSrc:"https://www.gartenmagazin.net/wp-content/uploads/2014/03/10-03-2014-09-46-20.jpg",
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title:  "Gurke",
     country: "portugal",
     price: 2.70,
-    taste: "fruchtig",
     description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
     gin: ["Gin Mare", "Teuer Gin"]
   },
   {
-    imgSrc:"https://praxistipps-images.chip.de/2FYqpf5fQIpZZGGesA-0Ufq-y6s=/0x0/filters:format(jpeg):fill(fff,true):no_upscale()/praxistipps.s3.amazonaws.com%2Flimette-und-zitrone-unterschiede-in-der-haltbarkeit-bild-pixabay-_8b0b4a23.jpg",
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Limette",
     country: "deutschland",
     price: 29.99,
-    taste: "kräftig",
     description: "Die Algarve in Portugal mit ihren vielen Sonnentagen, dem heißen und trockenen Sommer und der sanften Meerbrise gilt als die südlichste Region auf dem portogiesischen Festland und spielt bei dem Gin Sul eine große Rolle. Gleiches gilt auch für die Sonderedition von 2016, den Gin Sul in der Edition Rota do Sul. Die Hersteller dieses Gins waren von der Vegetation der portogiesischen Küste so begeistert, dass sie sich kurzerhand aufmachten, um in der Region nach weiteren Botanicals zu suchen, die sie für ihre Gins verwenden konnten. Die Zistrose, eine Art Strauch, wächst im heißen Klima des Mittelmeers und an der Atlantikküste Portugals. Diese Zutat findet bereits im klassischen Gin Sul Verwendung. Da verwundert es nicht, dass die Hersteller des Gins für den Rota do Sul erneut die sonnenbeschienene Region aufsuchten. Die ausgewählten Botanicals für diesen Gin spiegeln das Flair der portogiesischen Küste perfekt wider. Oregano, Thymianblüten und Rosmarin verleihen dem Rota do Sul eine kräuterige Note, während Strohblumen den Gin mit ihren floralen Aromen untermalen. Frische Zitronen, Apfelsinen und Aprikosen sorgen für eine angenehme Frische, die nicht nur im heißen Klima der Algarve äußerst willkommen ist. In der Altoaner Spirituosen Manufaktur in Hamburg wurde schließlich klares Quellwasser aus dem Monchique-Gebirge hinzugegeben, das dem Rota do Sul noch mehr Exklusivität verleiht. Der Gin ist außerdem auf nur 2.000 Flaschen limitiert.",
     gin: ["Teuer Gin","Gin Sul Edition Rota do Sul"]
   },
   {
-    imgSrc:"https://www.fitforfun.de/files/images/201503/7/fff-video-schlank-mit-zitrone,181912_1x1_n.jpg",
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     title: "Zitrone",
     country: "deutschland",
     price: 0.40,
@@ -130,14 +194,45 @@ export const ingredients = [
     gin: ["Gin Sul Edition Rota do Sul"]
   },
   {
-    imgSrc:"https://images.eatsmarter.de/sites/default/files/styles/facebook/public/zimt_576x432.jpg",
-    title: "Zimt",
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Orange",
     country: "portugal",
     price: 89,
-    taste: "fruchtig",
     description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
     gin: ["Teuer Gin"]
   },
+  {
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Apfel",
+    country: "portugal",
+    price: 89,
+    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
+    gin: ["Teuer Gin"]
+  },
+  {
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Birne",
+    country: "portugal",
+    price: 89,
+    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
+    gin: ["Teuer Gin"]
+  },
+  {
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Grapefrut",
+    country: "portugal",
+    price: 89,
+    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
+    gin: ["Teuer Gin"]
+  },
+  {
+    imgSrc: "https://images.unsplash.com/photo-1532336414038-cf19250c5757?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    title: "Ingwer",
+    country: "portugal",
+    price: 89,
+    description: "Angelikawurzel, Ingwer, Kardamom, Koriandersamen, Lakritz, Wacholder, Zimt",
+    gin: ["Teuer Gin"]
+  }
 ];
 
 const dummi =[
